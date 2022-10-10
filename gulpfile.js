a18d5225830926c27a889b931a0ded180230aba4
Lucas Pirolli <lucas.pirolli@outlook.com>
2022-10-09 21:16:00 -0300

    wip: added gulp compression, color adjustment, and information addition.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,43 @@
 "use strict";
+
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const browserSync = require("browser-sync").create();
 
 sass.compiler = require("node-sass");
 
-gulp.task('default', watch);
-gulp.task('sass', compileSass);
+gulp.task("default", watch);
+gulp.task("compact", compileSassCompact);
+gulp.task("compressed", compileSassCompressed);
 
-function compileSass(){
-  return gulp.src('src/scss/app.scss')
-  .pipe(sass({
-    outputStyle: 'compact'
-  })).on('error', sass.logError)
-  .pipe(gulp.dest('dist/assets/css/'))
-  .pipe(browserSync.stream())
+function compileSassCompact() {
+  return gulp
+    .src("src/scss/app.scss")
+    .pipe(
+      sass({
+        outputStyle: "compact",
+      })
+    )
+    .on("error", sass.logError)
+    .pipe(gulp.dest("dist/assets/css/"));
+}
+
+function compileSassCompressed() {
+  return gulp
+    .src("src/scss/app.scss")
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+      })
+    )
+    .on("error", sass.logError)
+    .pipe(gulp.dest("dist/assets/css/"));
 }
 
 function watch() {
-  gulp.watch('src/scss/**/*.scss', compileSass); 
-  gulp.watch('dist/*.html', compileSass);
-  gulp.watch('src/js/**/*.js', compileSass)
+  gulp.watch("src/scss/**/*.scss", compileSassCompact);
+  gulp.watch("dist/*.html", compileSassCompact);
+  gulp.watch("src/js/**/*.js", compileSassCompact);
+  gulp.watch("src/scss/**/*.scss", compileSassCompressed);
+  gulp.watch("dist/*.html", compileSassCompressed);
+  gulp.watch("src/js/**/*.js", compileSassCompressed);
 }
